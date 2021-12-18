4a890b8a151f5133e5fd1de170a2199e9d6eee1c
AlaaSaadallah <73827903+AlaaSaadallah@users.noreply.github.com>
2021-12-18 16:52:20 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "root";
+$username = "";
+$password = "";
 $dbname = "enozom";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
